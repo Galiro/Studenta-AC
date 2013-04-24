398814b707010a520f310cc11887a74944977178
Galiro <galiro@galiro.com>
2013-04-23 20:04:26 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StudentaAc::Application.config.secret_token = '5265e616012ef306a9a85621214f6a641a8203b46c3e5c5ff4a1356463c8b031cdc9ec3e33b58916d4a92aa05d37d2c3fab21cb5fca12fd0cc99c2a5c6c4a937'
